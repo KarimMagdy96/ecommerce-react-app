9df3fd6a93e08f970c35a92343cc05c9ec71c61c
Karim <karimmagdy096@gmail.com>
2024-05-03 21:03:14 +0300

    update

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,5 +1,23 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export const Banner = () => {
-  return <div className="hero-banner">Banner</div>;
+  return (
+    <div className="banner">
+      <div className="hero-banner">
+        <div className="hero--content">
+          <div className="content">
+            <h2>Crafting your Exceptional Dining Reservations</h2>
+            <p>
+              Reservation is a step into a world of gastronomic wonder. Reserve
+              your table today and let us paint your culinary dreams into
+              reality.
+            </p>
+          </div>
+          <div className="btn">
+            <Link to="/">Explore Products</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -3,11 +3,14 @@ import { Navbar } from "../navbar/Navbar";
 import { Footer } from "./../footer/Footer";
 import { Outlet } from "react-router-dom";
 import { MobileMenu } from "../mobilMenu/MobileMenu";
+import { Banner } from "../banner/Banner";
+
 export const Layout = () => {
   return (
     <>
       <MobileMenu />
       <Navbar />
+      <Banner />
       <Outlet />
       <Footer />
     </>
